4ac7929a0fe68c50221e1166a37ed9f5493c1449
AlexGarbus <alex.garbus@outlook.com>
2022-04-02 15:25:52 -0400

    Player animation

--- player/player.gd
+++ player/player.gd
@@ -1,12 +1,27 @@
+class_name Player
 extends KinematicBody2D
 
 
 export var move_speed := 1.0
 
+onready var sprite := $AnimatedSprite
+
 
 func _physics_process(delta: float) -> void:
-	move_and_slide(_get_move_direction() * move_speed)
+	var direction = _get_move_direction()
+	
+	move_and_slide(direction * move_speed)
+
+	var animation = _get_animation(direction)
+	if not sprite.animation == animation:
+		sprite.play(animation)
 
 
 func _get_move_direction() -> Vector2:
 	return Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 0)
+
+
+func _get_animation(direction: Vector2) -> String:
+	if not direction.x == 0:
+		return "move"
+	return "idle"
